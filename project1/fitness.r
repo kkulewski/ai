607bcac4ac81ee49567f5861be68f6501133acf3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-10-29 22:27:24 +0100

    Extract fitness function

--- project1/fitness.r
+++ project1/fitness.r
@@ -1,27 +1,10 @@
-
-chr1 = c(1, 0, 1)
-
-# 3/3 satisfable with chr1
-cnf1 = list()
-cnf1[[1]] = c(1, 2, -3)
-cnf1[[2]] = c(1, -2, -3)
-cnf1[[3]] = c(-1, 3)
-
-# 3/5 satisfable with chr1
-cnf2 = list()
-cnf2[[1]] = c(1, 2, -3)
-cnf2[[2]] = c(-1, 2, -3)
-cnf2[[3]] = c(1, -2, -3)
-cnf2[[4]] = c(-1)
-cnf2[[5]] = c(-1, 3)
-
+# measure chromosome fitness for given CNF formula
 CnfFitness = function(cnf, chromosome)
 {
   clausesToSatisfy = length(cnf)
   
   for (clauseNumber in 1:length(cnf))
   {
-    
     clauseSatisfied = FALSE
     clause = cnf[[clauseNumber]]
     
@@ -37,18 +20,13 @@ CnfFitness = function(cnf, chromosome)
       {
         if (chromosome[-variable] == 0) { clauseSatisfied = TRUE }
       }
-      
     }
     
     if (clauseSatisfied == TRUE)
     {
       clausesToSatisfy = clausesToSatisfy - 1
     }
-    
   }
   
   return (clausesToSatisfy)
 }
-
-CnfFitness(cnf1, chr1)
-CnfFitness(cnf2, chr1)
