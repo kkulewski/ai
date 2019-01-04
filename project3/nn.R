02ab53dec2301fbcb7d026563375fe35624aaad0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-01-05 00:23:49 +0100

    Data frame column selection

--- project3/nn.R
+++ project3/nn.R
@@ -39,3 +39,40 @@ write.csv(data.fixed, file = "data-fixed.csv")
 # Process #
 ###########
 
+data.stripped = data.fixed[, c(10:16, 50, 1:9, 29:36)]
+write.csv(data.stripped, file = "data-stripped.csv")
+
+# INPUT
+# 01 myTank.controls.turnLeft
+# 02 myTank.controls.turnRight
+# 03 myTank.controls.goForward
+# 04 myTank.controls.goBack
+# 05 myTank.controls.shoot
+# 06 myTank.controls.cannonLeft
+# 07 myTank.controls.cannonRight
+
+# TIME
+# 08 currentGameTime
+
+# MY TANK
+# 09 myTank.x
+# 10 myTank.y
+# 11 myTank.rotation
+# 12 myTank.cannonRotation
+# 13 myTank.velocityX
+# 14 myTank.velocityY
+# 15 myTank.accelerationX
+# 16 myTank.accelerationY
+# 17 myTank.shootCooldown
+
+# ENEMY TANK
+# 18 enemyTank.x
+# 19 enemyTank.y
+# 20 enemyTank.rotation
+# 21 enemyTank.cannonRotation
+# 22 enemyTank.velocityX
+# 23 enemyTank.velocityY
+# 24 enemyTank.accelerationX
+# 25 enemyTank.accelerationY
+
+
